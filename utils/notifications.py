e2e99c92e6229100fb3933fa3f1291b3e8654d83
AnDuquenne <andreas_duquenne@hotmail.com>
2024-04-17 15:28:27 +0200

    Update notifs to send to telegram group

--- utils/notifications.py
+++ utils/notifications.py
@@ -1,5 +1,17 @@
 import asyncio
 import telegram_send
+from telegram import Bot
+
+# Import env variables
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("bot_token")
+
+bot = Bot(token=token)
+
+group_chat_id = '-4109659430'
 
 
 async def send_telegram_message(title, message):
@@ -10,11 +22,16 @@ async def send_telegram_message(title, message):
 
     msg += f"{message}"
 
-    await telegram_send.send(messages=[msg])
+    await bot.send_message(chat_id=group_chat_id, text=msg)
 
     pass
 
 
 def send_message(title, message):
     asyncio.get_event_loop().run_until_complete(send_telegram_message(title, message))
+    pass
+
+
+if __name__ == "__main__":
+    send_message("Test", "This is a test message")
     pass
