12119db9251a41a82cfcfefa29eed727fa323361
AnDuquenne <andreas_duquenne@hotmail.com>
2024-04-18 03:05:21 +0200

    change settings

--- live_testing/live_test_main.py
+++ live_testing/live_test_main.py
@@ -6,7 +6,7 @@ import datetime
 
 run_name = "ETH_USD_" + datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
 
-strategy = SFStrategyI(run_name=run_name, buy_percentage=0.00025)
+strategy = SFStrategyI(run_name=run_name, buy_percentage=0.0025)
 wallet = 1000
 
 if __name__ == "__main__":
