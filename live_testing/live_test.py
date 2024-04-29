5d41fbf4a2c3dcd02cb9e8e2b97347ad92b730a3
AnDuquenne <andreas_duquenne@hotmail.com>
2024-04-30 00:49:47 +0200

    Live test for FC and LSTM, with defining strategies

--- live_testing/live_test.py
+++ live_testing/live_test.py
@@ -249,43 +249,86 @@ class LiveTest():
             if t.second == 0:
                 notif_sent = False
 
-            if t.second == 30 and t.minute % 2 == 0 and not notif_sent:
-                title = emoji.emojize(":clock1:") + " Recurrent update " + emoji.emojize(":clock1:") + "\n\n"
-                msg = ""
-                msg += f"Positions:\n"
-                msg += f"\t\t open: {open_pos_}\n"
-                msg += f"\t\t closed: {len(self.positions) - open_pos_}\n"
-                msg += f"\t\t total: {len(self.positions)}\n"
-                msg += f"PNL of positions\n"
-                msg += f"\t\t Closed: {pnl_closed}\n"
-                msg += f"\t\t Open: {pnl_open}\n"
-                msg += f"\t\t Total: {pnl_closed + pnl_open}\n"
-                msg += f"Total value of positions: {tot_}\n"
-                msg += f"Total value of the wallet (pos+wallet): {tot_ + self.wallet}\n"
-                send_message(title, msg)
-                notif_sent = True
-                iter_log += 1
+            # Duplicate code because of different timing for networks
+            if (isinstance(self.strategy, SFStrategy) or
+                    isinstance(self.strategy, SFStrategyI) or
+                    isinstance(self.strategy, StrategyMACD)):
+                if t.second == 30 and t.minute % 2 == 0 and not notif_sent:
+                    title = emoji.emojize(":clock1:") + " Recurrent update " + emoji.emojize(":clock1:") + "\n\n"
+                    msg = ""
+                    msg += f"Positions:\n"
+                    msg += f"\t\t open: {open_pos_}\n"
+                    msg += f"\t\t closed: {len(self.positions) - open_pos_}\n"
+                    msg += f"\t\t total: {len(self.positions)}\n"
+                    msg += f"PNL of positions\n"
+                    msg += f"\t\t Closed: {pnl_closed}\n"
+                    msg += f"\t\t Open: {pnl_open}\n"
+                    msg += f"\t\t Total: {pnl_closed + pnl_open}\n"
+                    msg += f"Total value of positions: {tot_}\n"
+                    msg += f"Total value of the wallet (pos+wallet): {tot_ + self.wallet}\n"
+                    send_message(title, msg)
+                    notif_sent = True
+                    iter_log += 1
 
-                if env == "server":
-                    # Record pnl and wallet each 10 minutes
-                    # Check if file exists
-                    if not os.path.exists("io/live_test/perf/" + self.strategy.__str__() + ".csv"):
-                        subprocess.run(["touch", "io/live_test/perf/" + self.strategy.__str__() + ".csv"])
-                        with open("io/live_test/perf/" + self.strategy.__str__() + ".csv", "w") as f:
-                            f.write("iter,time,open_pos,closed_pos,tot_pos,"
-                                    "pnl_pos_closed,pnl_pos_open,pnl_pos_tot,wallet_tot\n")
+                    if env == "server":
+                        # Record pnl and wallet each 10 minutes
+                        # Check if file exists
+                        if not os.path.exists("io/live_test/perf/" + self.strategy.__str__() + ".csv"):
+                            subprocess.run(["touch", "io/live_test/perf/" + self.strategy.__str__() + ".csv"])
+                            with open("io/live_test/perf/" + self.strategy.__str__() + ".csv", "w") as f:
+                                f.write("iter,time,open_pos,closed_pos,tot_pos,"
+                                        "pnl_pos_closed,pnl_pos_open,pnl_pos_tot,wallet_tot\n")
 
-                    # Write the data
-                    with open("io/live_test/perf/" + self.strategy.__str__() + ".csv", "a") as f:
-                        f.write(f"{iter_log},"
-                                f"{t},"
-                                f"{open_pos_},"
-                                f"{len(self.positions) - open_pos_},"
-                                f"{len(self.positions)},"
-                                f"{pnl_closed},"
-                                f"{pnl_open},"
-                                f"{pnl_closed + pnl_open},"
-                                f"{tot_ + self.wallet}\n")
+                        # Write the data
+                        with open("io/live_test/perf/" + self.strategy.__str__() + ".csv", "a") as f:
+                            f.write(f"{iter_log},"
+                                    f"{t},"
+                                    f"{open_pos_},"
+                                    f"{len(self.positions) - open_pos_},"
+                                    f"{len(self.positions)},"
+                                    f"{pnl_closed},"
+                                    f"{pnl_open},"
+                                    f"{pnl_closed + pnl_open},"
+                                    f"{tot_ + self.wallet}\n")
+
+            elif isinstance(self.strategy, StrategyFC) or isinstance(self.strategy, StrategyLSTM):
+                if t.minute % 2 == 0 and not notif_sent:
+                    title = emoji.emojize(":clock1:") + " Recurrent update " + emoji.emojize(":clock1:") + "\n\n"
+                    msg = ""
+                    msg += f"Positions:\n"
+                    msg += f"\t\t open: {open_pos_}\n"
+                    msg += f"\t\t closed: {len(self.positions) - open_pos_}\n"
+                    msg += f"\t\t total: {len(self.positions)}\n"
+                    msg += f"PNL of positions\n"
+                    msg += f"\t\t Closed: {pnl_closed}\n"
+                    msg += f"\t\t Open: {pnl_open}\n"
+                    msg += f"\t\t Total: {pnl_closed + pnl_open}\n"
+                    msg += f"Total value of positions: {tot_}\n"
+                    msg += f"Total value of the wallet (pos+wallet): {tot_ + self.wallet}\n"
+                    send_message(title, msg)
+                    notif_sent = True
+                    iter_log += 1
+
+                    if env == "server":
+                        # Record pnl and wallet each 10 minutes
+                        # Check if file exists
+                        if not os.path.exists("io/live_test/perf/" + self.strategy.__str__() + ".csv"):
+                            subprocess.run(["touch", "io/live_test/perf/" + self.strategy.__str__() + ".csv"])
+                            with open("io/live_test/perf/" + self.strategy.__str__() + ".csv", "w") as f:
+                                f.write("iter,time,open_pos,closed_pos,tot_pos,"
+                                        "pnl_pos_closed,pnl_pos_open,pnl_pos_tot,wallet_tot\n")
+
+                        # Write the data
+                        with open("io/live_test/perf/" + self.strategy.__str__() + ".csv", "a") as f:
+                            f.write(f"{iter_log},"
+                                    f"{t},"
+                                    f"{open_pos_},"
+                                    f"{len(self.positions) - open_pos_},"
+                                    f"{len(self.positions)},"
+                                    f"{pnl_closed},"
+                                    f"{pnl_open},"
+                                    f"{pnl_closed + pnl_open},"
+                                    f"{tot_ + self.wallet}\n")
 
             if env == "local":
                 time.sleep(5 + waiting_time)
