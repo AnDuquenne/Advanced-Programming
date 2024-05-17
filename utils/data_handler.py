7ff865ca7480b5304e2f9fecc261335a2c08e590
AnDuquenne <andreas_duquenne@hotmail.com>
2024-05-17 19:09:38 +0200

    Update data_handler.py

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -1,10 +1,10 @@
-import os, sys
-# Get the current script's directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# Get the parent directory by going one level up
-parent_dir = os.path.dirname(current_dir)
-# Add the parent directory to sys.path
-sys.path.append(parent_dir)
+# import os, sys
+# # Get the current script's directory
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# # Get the parent directory by going one level up
+# parent_dir = os.path.dirname(current_dir)
+# # Add the parent directory to sys.path
+# sys.path.append(parent_dir)
 
 import pandas as pd
 import numpy as np
